597625650e1cae0c0065dbad33832b8e6f4b0d90
Uduak Ben <uduakben11@gmail.com>
2023-07-29 20:21:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+let pass = document.getElementById("password");
+let strength = document.getElementById("strength");
+let message = document.getElementById("message");
+
+
+pass.addEventListener("input", () =>{
+    if(pass.value.length > 0){
+        message.style.display = "block"
+    }
+    else{
+        message.style.display ="none";
+    }
+    if(pass.value.length < 4){
+        strength.innerHTML = "weak"
+        pass.style.borderColor = "red";
+        message.style.color = "red";
+        
+    }
+    else if(pass.value.length >= 4 && pass.value.length < 8){
+        strength.innerHTML = "medium";
+        pass.style.borderColor = "yellow";
+        message.style.color = "yellow";
+        
+    }
+    else if(pass.value.length >= 8){
+        strength.innerHTML = "strong";
+        pass.style.borderColor = "green";
+        message.style.color = "green";
+    }
+})
